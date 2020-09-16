cd60be6c5ffed93f668fb8ba3899e3b49159d353
deepthi <deepthi@gmail.com>
2020-09-16 11:39:09 +0530

    https://github.com/Deepthi-bathula/practice.git

---
+++ Js-basics/index1.js
@@ -0,0 +1,55 @@
+// var sub=function(a,b){return a-b;}
+// console.log(sub(15,12));
+
+// var mul=function(a,b){return a*b;}
+// console.log(mul(2,15));
+
+
+var add=function(){
+    var ar=arguments;
+    var res=0;
+for(var i=0;i<ar.length;i++){
+res += ar[i];
+}
+return res;
+}
+
+var sub=function(){
+    var ar=arguments;
+    var res=ar[0];
+    for(var i=1;i<ar.length;i++)
+    {
+    res = res-ar[i];
+    }
+    return res;
+}
+
+var mul=function(){
+    var ar=arguments;
+    var res=ar[0];
+    for(var i=1;i<ar.length;i++)
+    {
+    res = res*ar[i];
+    }
+    return res;
+}
+
+var div=function(){
+    var ar=arguments;
+    var res=ar[0];
+    for(var i=1;i<ar.length;i++)
+    {
+    res = res/ar[i];
+    }
+    return res;
+}
+
+var mod=function(){
+    var ar=arguments;
+    var res=ar[0];
+    for(var i=1;i<ar.length;i++)
+    {
+    res %= ar[i];
+    }
+    return res;
+}
